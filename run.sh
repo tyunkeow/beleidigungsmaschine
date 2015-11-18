b6619443cb4b1b20a2900fcb47f90fb035966334
tyunkeow <urlaub.harms@web.de>
2015-11-18 19:51:16 +0100

    added missing ADD statement

--- run.sh
+++ run.sh
@@ -11,9 +11,13 @@ docker rm brickd
 docker rm insulter
 
 echo "Starting docker containers..."
+# start syslog daemon container
 docker run -d --privileged --name syslog -v /tmp/syslogdev:/dev syslog
 docker run -d --privileged --name brickd tinkerforge_brickd
 docker run -d --privileged --name insulter --link brickd:brickd insulter
 docker ps -a
+
+echo "The following files were bound to the host:"
+docker inspect -f {{.Volumes}} syslog
 
 echo "done."
