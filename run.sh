2f3069b7eb09d0d728db60a856d408ac76fb4137
tyunkeow <urlaub.harms@web.de>
2015-11-21 09:07:52 +0100

    Fixed link to audio db. Must be relative

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+echo "This script is located at $SCRIPT"
+
 echo "Stopping existing docker containers..."
 docker stop insulter
 docker stop brickd
@@ -14,7 +18,7 @@ echo "Starting docker containers..."
 # start syslog daemon container
 docker run -d --privileged --name syslog -v /tmp/syslogdev:/dev syslog
 docker run -d --privileged --name brickd tinkerforge_brickd
-docker run -d --privileged --name insulter --link brickd:brickd -v ./insulter/audio_db:/insulter/audio_db insulter
+docker run -d --privileged --name insulter --link brickd:brickd -v "${SCRIPT}/insulter/audio_db:/insulter/audio_db" insulter
 docker ps -a
 
 echo "The following files were bound to the host:"
