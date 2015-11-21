eacda127dc748115dfe6b2f2dc3f0ac0a3e8f2b0
tyunkeow <urlaub.harms@web.de>
2015-11-21 09:13:21 +0100

    Fixed path to audio db

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
-echo "This script is located at $SCRIPT"
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo "This script is located at $SCRIPTPATH"
 
 echo "Stopping existing docker containers..."
 docker stop insulter
@@ -18,7 +19,7 @@ echo "Starting docker containers..."
 # start syslog daemon container
 docker run -d --privileged --name syslog -v /tmp/syslogdev:/dev syslog
 docker run -d --privileged --name brickd tinkerforge_brickd
-docker run -d --privileged --name insulter --link brickd:brickd -v "${SCRIPT}/insulter/audio_db:/insulter/audio_db" insulter
+docker run -d --privileged --name insulter --link brickd:brickd -v "${SCRIPTPATH}/insulter/audio_db:/insulter/audio_db" insulter
 docker ps -a
 
 echo "The following files were bound to the host:"
