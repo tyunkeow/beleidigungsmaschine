ba0068327a58852695d5311335e7fb6055463780
tyunkeow <urlaub.harms@web.de>
2015-11-21 12:45:31 +0100

    renamed docker image insulter to insultr

--- insulter/build_insult_db.py
+++
@@ -1,8 +0,0 @@
-from insulter import Insulter
-
-if __name__ == "__main__":
-    insulter = Insulter()
-    insulter.create_insult_audio_db()
-
-    insulter.speak_next_insult("m")
-    insulter.speak_next_insult("f")
--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 echo "This script is located at $SCRIPTPATH"
 
 echo "Stopping existing docker containers..."
-docker stop insulter
+docker stop insultr
 docker stop brickd
 docker stop syslog
 
 echo "Removing existing docker containers..."
-docker rm insulter
+docker rm insultr
 docker rm brickd
 docker rm syslog
 
@@ -19,11 +19,11 @@ echo "Starting docker containers..."
 # start syslog daemon container
 docker run -d --privileged --name syslog -v /tmp/syslogdev:/dev syslog
 docker run -d --privileged --name brickd tinkerforge_brickd
-docker run -d --privileged --name insulter --link brickd:brickd -v "${SCRIPTPATH}/insult_db/:/insulter/insult_db" insulter
+docker run -d --privileged --name insultr --link brickd:brickd -v "${SCRIPTPATH}/insult_db/:/insulter/insult_db" insultr
 docker ps -a
 
 echo "The following files were bound to the host:"
 docker inspect -f {{.Volumes}} syslog
-docker inspect -f {{.Volumes}} insulter
+docker inspect -f {{.Volumes}} insultr
 
 echo "done."
