afe07463e359fb9b904de66614044a5fc1ac648d
tyunkeow <urlaub.harms@web.de>
2015-11-21 21:15:36 +0100

    fixed CMD in Dockerfile

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 echo "This script is located at $SCRIPTPATH"
 
-stop.sh
+./stop.sh
 
 echo "Starting docker containers..."
 # start syslog daemon container
 docker run -d --privileged --name syslog -v /tmp/syslogdev:/dev syslog
 docker run -d --privileged --name brickd tinkerforge-brickd
-docker run -d --privileged --name insultr --link brickd:brickd -v "${SCRIPTPATH}/insult_db/:/insulter/insult_db" insultr
+docker run -d --privileged --name insultr --link brickd:brickd -v "${SCRIPTPATH}/insult_db:/insultr/insult_db" insultr
 docker ps -a
 
 echo "The following files were bound to the host:"
