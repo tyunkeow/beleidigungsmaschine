b63b552e3f63869dd0445289e2e086276d84b440
tyunkeow <urlaub.harms@web.de>
2015-12-20 14:50:24 +0100

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -38,26 +38,26 @@ apt-get install -y --no-install-recommends \
     vim
 
 mkdir -p /tmp/build-insultr
-cd /tmp/build-insultr
+#cd /tmp/build-insultr
 
 # install pip
-curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
-python get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py > /tmp/build-insultr/get-pip.py
+python /tmp/build-insultr/get-pip.py
 
 # install tinkerforge
 pip install tinkerforge
 
 # install brickd 
-curl -SL "http://download.tinkerforge.com/tools/brickd/linux/brickd-2.2.2_armhf.deb" -o brickd.deb
-dpkg -i brickd.deb
-rm brickd.deb
-cp brickd /etc/init.d/
+curl -SL "http://download.tinkerforge.com/tools/brickd/linux/brickd-2.2.2_armhf.deb" -o /tmp/build-insultr/brickd.deb
+dpkg -i /tmp/build-insultr/brickd.deb
+rm /tmp/build-insultr/brickd.deb
+cp brickd.sh /etc/init.d/
 
 # install shairport
 mkdir -p /root/.ssh/
 ssh-keyscan github.com >> /root/.ssh/known_hosts
 
-git clone https://github.com/mikebrady/shairport-sync.git
+git clone https://github.com/mikebrady/shairport-sync.git /tmp/build-insultr/shairport-sync
 
 cd /tmp/build-insultr/shairport-sync && \
     autoreconf -i -f && \
